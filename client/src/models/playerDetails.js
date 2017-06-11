67458c74ad6f125d8b5c22bda61e994564facc0e
hatwell <caroline.hatwell@gmail.com>
2017-06-11 13:22:35 +0100

    created players info model

--- client/src/models/playerDetails.js
+++ client/src/models/playerDetails.js
@@ -1,25 +1,35 @@
 class PlayerDetails = function(){
-  this.PLAYERS_INFO = [
-    {
-      name: "LeBron James",
-     team: "Cleveland Cavaliers",
-     height:
-   },
-    {
-      name: "Steph Curry",
-     team: "Golden State Warriors",
-     height:
-   },
-    {
-      name: "Kevin Durant",
-     team: "Golden State Warriors",
-     height:
-   },
-    {
-      name: "Russell Westbrook",
-     team: "Oklahoma City Thunder",
-     height:
-   },
-
-  ]
+  constructor(){
+    this.PLAYERS_INFO = [
+      {
+        name: "LeBron James",
+       team: "Cleveland Cavaliers",
+       height: 203
+     },
+      {
+        name: "Steph Curry",
+       team: "Golden State Warriors",
+       height: 190
+     },
+      {
+        name: "Kevin Durant",
+       team: "Golden State Warriors",
+       height: 206
+     },
+      {
+        name: "Russell Westbrook",
+       team: "Oklahoma City Thunder",
+       height: 191
+     }
+    ]
+  }
 }
+
+getTeams(){
+  const allTeams = this.PLAYERS_INFO.map(function(player){
+    return player.team;
+  })
+  return allTeams;
+}
+
+export default PlayerDetails;
