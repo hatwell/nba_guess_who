ae1f1ad1956f4049e1cd13c3a72532017703b8d0
hatwell <caroline.hatwell@gmail.com>
2017-06-11 13:17:55 +0100

    first commit

---
+++ client/src/models/player.js
@@ -0,0 +1,8 @@
+class Player = function(options){
+  this.name = options.name;
+  this.team = options.team;
+  this.height = options.height;
+
+}
+
+export default Player;
---
+++ client/src/models/playerDetails.js
@@ -0,0 +1,25 @@
+class PlayerDetails = function(){
+  this.PLAYERS_INFO = [
+    {
+      name: "LeBron James",
+     team: "Cleveland Cavaliers",
+     height:
+   },
+    {
+      name: "Steph Curry",
+     team: "Golden State Warriors",
+     height:
+   },
+    {
+      name: "Kevin Durant",
+     team: "Golden State Warriors",
+     height:
+   },
+    {
+      name: "Russell Westbrook",
+     team: "Oklahoma City Thunder",
+     height:
+   },
+
+  ]
+}
